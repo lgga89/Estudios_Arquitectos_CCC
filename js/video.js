bd6e5e89243bcca4c419206d9274b32f02275bfb
Luis Garcia <lg_20_GA@hotmail.com>
2021-09-05 03:43:53 -0500

    responsive
    se esta adaptando a los diferentes dispositivos aun falta

--- js/video.js
+++
@@ -1,6 +0,0 @@
-//For Firefox we have to handle it in JavaScript
-var vids = $("video");
-$.each(vids, function () {
-  this.controls = false;
-});
-//Loop though all Video tags and set Controls as false
